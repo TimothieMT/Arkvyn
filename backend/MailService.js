0707765920ad5842f2e7f2eb9880c72f1732a804
timtolk <t.tolk@reputativ.com>
2026-03-04 21:53:57 +0100

    fix: dotenv path relative to __dirname

--- backend/MailService.js
+++ backend/MailService.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 app.use(cors({
